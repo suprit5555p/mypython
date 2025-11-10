d8cfb4df6d076dc8b21c68fd1d577a8ae5949566
suprit5555p <parushettisuprit@gmail.com>
2025-11-10 16:29:17 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+age = int(input("enter your age"))
+name = (input("enter your name"))
+weight = float(input("enter your weight"))
+addition=age+weight
+sub=age-weight
+div=age/weight
+div2=age//weight
+div3=age%weight
+mul=age*weight
+print(addition,sub,div,mul)
+print(div2)
+print(div3)
