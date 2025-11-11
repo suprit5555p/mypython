6498dc7e0acfbcbb29bb4eff9fed9eeb7ddd8457
suprit5555p <parushettisuprit@gmail.com>
2025-11-11 13:59:41 +0530

    Create leargest num.py

---
+++ leargest num.py
@@ -0,0 +1,9 @@
+#largest of two number
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+if a<b :
+  print("the largest number is", b)
+elif a>b :
+  print("the largest number is", a)
+else :
+  print("both numbers are equal")
