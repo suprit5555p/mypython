a4a281f8fdae07902c6a587e0583bfe001d8aa27
suprit5555p <parushettisuprit@gmail.com>
2025-11-11 14:03:37 +0530

    Add grade calculation based on marks input

---
+++ gradecalculation.py
@@ -0,0 +1,10 @@
+#grade calculation
+a = int(input("enter your marks"))
+if a >= 90 :
+  print("grade A")
+elif a >= 75 :
+  print("grade B")
+elif a >= 50 :
+  print("grade C")
+else :
+  print("fail")
