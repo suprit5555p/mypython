35a8261b22354feade0538cce988fbe557bc7219
suprit5555p <parushettisuprit@gmail.com>
2025-11-12 10:22:41 +0530

    Create formatedstring.py

---
+++ formatedstring.py
@@ -0,0 +1,6 @@
+#formatted string
+name = "suprit"
+age = 18
+college = "shridev"
+department = "AIML"
+print(f"my name is : {name} and my age is: {age} i am studying at : {college} in department of : {department}")
