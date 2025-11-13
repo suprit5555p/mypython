118d11fc1b0f83432be3c38ecbe48615ec3d8038
suprit5555p <parushettisuprit@gmail.com>
2025-11-13 12:08:18 +0530

    Add areaofcircle function to calculate circle area

---
+++ custummoduleareaofcircle.py
@@ -0,0 +1,9 @@
+def areaofcircle():
+  pi=3.14
+  r = int(input("Enter the radius of the circle: "))
+  area = pi*r*r
+  print("the area of circle is", area)
+
+
+  import modi_py
+modi_py.areaofcircle()
