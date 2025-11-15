661688c23fa94297486f6be3a22e29ec8cbaa824
suprit5555p <parushettisuprit@gmail.com>
2025-11-15 12:02:05 +0530

    Create oops.py

---
+++ oops.py
@@ -0,0 +1,9 @@
+#oops
+class areaofcircle:
+  def __init__(self, radius):
+    self.radius = radius
+  def function(self):
+    return (3.14*self.radius*self.radius)
+
+object=areaofcircle(10)
+print(object.function())
